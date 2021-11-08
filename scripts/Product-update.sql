63e1eea68f8169a8100aa548279f6a95f309b816
ramumtaz <rashid@mentoringminds.com>
2021-11-08 12:48:53 +0500

    add script for issue#1

---
+++ scripts/Product-update.sql
@@ -0,0 +1,8 @@
+  alter table [Pharmacy].[dbo].[tbl_Product]
+  add [purchaseRate] [decimal](18, 2) NULL
+
+  alter table [Pharmacy].[dbo].[tbl_Product]
+  add [salesRate] [decimal](18, 2) NULL
+
+  alter table [Pharmacy].[dbo].[tbl_Product]
+  add [Packing] [decimal](18, 2) NULL
